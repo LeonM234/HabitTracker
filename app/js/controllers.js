e4a6174f496794edd73053d49039eeb4d93ba0ea
Leon Peck <leonpeck234@gmail.com>
2014-12-17 09:13:15 -0600

    Angular routing implemented for nav links

---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+
+  angular.module('trackerApp')
+    .controller('HabitController', function($http, $routeParams){
+      var vm = this;
+      var id = $routeParams.id;
+    })
+
+}());
---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+(function() {
+  "use strict";
+
+  angular.module('trackerApp', ['ngRoute', 'mgcrea.ngStrap', 'ngAnimate'])
+
+}());
